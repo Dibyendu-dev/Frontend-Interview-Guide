5b019b05fd709cbaf6f795eee4c2896692cfa9cf
ddas4548 <ddas4548@gmail.com>
2025-09-01 22:41:39 +0530

    toast message  completed

--- 2-MachineCoding/09-toast/src/components/ToastContainer.jsx
+++ 2-MachineCoding/09-toast/src/components/ToastContainer.jsx
@@ -1,31 +1,44 @@
-import { useState } from "react"
-import "./style.css"
+import { useRef, useState } from "react";
+import "./style.css";
 
 export default function ToastContainer() {
-    const [show,setShow] = useState();
-    const handleClose = ()=>{
-        setShow(false)
-    }
-    const handleAdd = () => {
-        setShow(true);
-        setTimeout(()=>{
-            handleClose()
-        },3000)
-    }
+  const [toasts, setToasts] = useState([]);
+  const timerRef = useRef({})
+  const handleClose = (id) => {
+    clearTimeout(timerRef.current[id])
+    delete timerRef.current[id];
+    setToasts((prevToast) => {
+
+        const filteredToasts = prevToast.filter((toast) =>{
+        return toast.id !== id
+        })
+        return filteredToasts;
+    })
+  };
+  const handleAdd = (message, type) => {
+    const id = new Date().getTime();
+    const newToasts = [...toasts, { id, message, type }];
+    setToasts(newToasts);
+    timerRef.current[id] =setTimeout(()=> handleClose(id),5000);
+  };
 
   return (
     <div className="container">
-        <div className="toast-container">
-            {show && (<div className="toast">
-                Success <span onClick={handleClose}>x</span>
-            </div>)}
-        </div>
-        <div className="btn-container">
-            <button onClick={handleAdd}>Success</button>
-            <button>Info</button>
-            <button>Warning</button>
-            <button>Danger</button>
-        </div>
+      <div className="toast-container">
+        {toasts.map(({id,message,type}) => {
+          return(
+            <div key={id} className={`toast ${type}`} >
+            {message}<span onClick={()=>handleClose(id)}>x</span>
+          </div>
+          );
+        })}
+      </div>
+      <div className="btn-container">
+        <button onClick={() => handleAdd("Success", "success")}>Success</button>
+        <button onClick={() => handleAdd("Info", "info")}>Info</button>
+        <button onClick={() => handleAdd("Warning", "warning")}>Warning</button>
+        <button onClick={() => handleAdd("Danger", "danger")}>Danger</button>
+      </div>
     </div>
-  )
+  );
 }
