503468c2df830fc05ce83aab4a6f27714914a11a
ddas4548 <ddas4548@gmail.com>
2025-09-04 17:26:26 +0530

    implenting hover value in  star components

--- 2-MachineCoding/10-starRating/src/component/StarRating.jsx
+++ 2-MachineCoding/10-starRating/src/component/StarRating.jsx
@@ -2,15 +2,32 @@ import { useState } from "react";
 
 const StarRating = ({ starCount }) => {
   const [starvalue, setStarValue] = useState();
-  console.log(starvalue);
+  const [hovervalue, setHovervalue] = useState(0);
+
   return (
     <div className="star">
       {new Array(starCount).fill(0).map((val, index) => {
-        return <span
-         key={index}
-         className={ index < starvalue ? "active" : ""}
-        onClick={()=>{setStarValue(index+1)}}
-        >&#9733;</span>;
+        return (
+          <span
+            key={index}
+            className={
+              (hovervalue == 0 && index < starvalue) || index < hovervalue
+                ? "active"
+                : ""
+            }
+            onClick={() => {
+              setStarValue(index + 1);
+            }}
+            onMouseEnter={() => {
+              setHovervalue(index + 1);
+            }}
+            onMouseLeave={() => {
+              setHovervalue(0);
+            }}
+          >
+            &#9733;
+          </span>
+        );
       })}
     </div>
   );
