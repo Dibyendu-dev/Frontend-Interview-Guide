cc06fc2110f4474fedafa6350e98d60b547ff262
ddas4548 <ddas4548@gmail.com>
2025-09-08 17:13:47 +0530

    object static method id added

--- 1-Javascript/02-object/index.js
+++ 1-Javascript/02-object/index.js
@@ -77,10 +77,86 @@ for(let key in profile){
 let f1 = {name: "mango"}
 let f2 = {name: "mango"}
 
-// console.log(f1==f2)
-// console.log(f1===f2)
+//  
 
  f1 = f2
 
 // console.log(f1==f2)
 // console.log(f1===f2)
+
+// object static method
+
+const target = {a:1,b:2};
+const source = {c:3,d:4};
+
+const com = Object.assign(target,source)
+// console.log(com);
+// console.log(target);
+// console.log(source);
+
+const obj = {name: "ddas"};
+const obj2 = Object.assign({},obj);
+
+// console.log(obj2)
+// console.log(obj===obj2)
+
+const obj3={ a:1,b:{c:2}};
+const obj4 = Object.assign({},obj3)
+// console.log(obj4)
+// obj4.b.c =5
+
+// console.log(obj3.b.c)
+// console.log(obj4.b.c)
+
+const obj5 = structuredClone(obj3);
+
+obj5.b.c =9
+// console.log(obj5.b.c)
+// console.log(obj3.b.c)
+
+const myobj = {
+    name: "ddas",
+    age:29
+}
+const myarr = Object.entries(myobj)
+// console.log(myarr);
+
+const entries = new Map([
+    ["foo", "bar","ddas"],
+    ["baz", 42, 13],
+]);
+const objEntries = Object.fromEntries(entries)
+// console.log(objEntries);
+
+
+const emp = {
+    sal: 100
+}
+
+Object.freeze(emp);
+
+emp.sal = 200;
+emp.name = "Alex";
+delete emp.sal;
+
+console.log(emp)
+
+console.log(Object.isFrozen(emp));
+
+
+const dept = {
+    name: "finance"
+}
+
+Object.seal(dept);
+
+dept.address = "Bangalore"
+delete dept.name;
+
+dept.name = "HR"
+console.log(dept)
+
+// hasOwn
+
+console.log(Object.hasOwn(dept, "address"))
+console.log(Object.hasOwn(myobj, "age"))
