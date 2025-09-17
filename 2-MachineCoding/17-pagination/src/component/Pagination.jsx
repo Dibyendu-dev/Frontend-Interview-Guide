7d6a8bc9d04f33ae1cb414c50009f06c85593837
ddas4548 <ddas4548@gmail.com>
2025-09-17 21:21:00 +0530

    paginatation implemented

--- 2-MachineCoding/17-pagination/src/component/Pagination.jsx
+++ 2-MachineCoding/17-pagination/src/component/Pagination.jsx
@@ -1,23 +1,40 @@
+const Pagination = ({ pageNo, setPageNo }) => {
+  const prevThreeArr = Array.from(
+    { length: 3 },
+    (_, index) => pageNo - 1 - index
+  )
+    .filter((value) => value > 0)
+    .reverse();
 
-const Pagination = ({pageNo, setPageNo}) => {
+  const nextThreeArr = Array.from({ length: 3 }, (_, index) => pageNo + index);
 
-  
+  const paginationArr = [...prevThreeArr, ...nextThreeArr];
 
-  const handleNext = () =>{
-  setPageNo(pageNo+1)
-  }
+  const handleNext = () => {
+    setPageNo(pageNo + 1);
+  };
 
-  const handlePrev = () =>{
-    setPageNo(pageNo-1)
-  }
+  const handlePrev = () => {
+    setPageNo(pageNo - 1);
+  };
   return (
     <div className="pagination-container">
-      {pageNo >1 ? (<div  onClick={handlePrev} className="page-btn">{"<"}</div>): ("")}
-      
-      <div className="page-btn">{pageNo}</div>
-      <div onClick={handleNext} className="page-btn">{">"}</div>
-    </div>
-  )
-}
+      {pageNo > 1 ? (
+        <div onClick={handlePrev} className="page-btn">
+          {"<"}
+        </div>
+      ) : (
+        ""
+      )}
+      {paginationArr.map((val) => {
+        return <div onClick={()=>setPageNo(val)} className={val === pageNo ? `page-btn active` : `page-btn`}>{val}</div>;
+      })}
 
-export default Pagination
+      <div onClick={handleNext} className="page-btn">
+        {">"}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
